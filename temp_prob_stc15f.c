c9b219776e23668b3b485532924a3e4fe5aff7fa
Yao Fei <feiyao@me.com>
2013-08-07 00:21:56 +0800

    测试通过。 不再使用printf, 自己编写打印数字函数 定时器中断挂在1

--- temp_prob_stc15f.c
+++ temp_prob_stc15f.c
@@ -6,98 +6,115 @@
  *
  *  Yao Fei  feiyao@me.com
  */
-//#include <8052.h>
+
 #include <stc12.h>
-#include <stdio.h>
-
-#include "soft_uart.h"
-#include "ds18b20_1t.h"
-
-BYTE TPH, TPL;
 
 typedef unsigned char uchar;
 
-BYTE  flag;   // 是否采样标志
+// 仿真串口
+#include "soft_uart.h"
 
-char const hexs[] = "0123456789ABCDEF";
+// 温度传感器
+#include "ds18b20_1t.h"
+
+// 记录温度数据
+uchar TPH, TPL;
+uchar flag;   // 定时采样标志
 
 // 小数部分
-char* const digis[16]= {"0", "06", "13", "19", 
-			"25", "31", "38", "44", 
-			"5",  "56", "63", "69",
-			"75", "81", "88", "94"};
+char const __code digis[16]= {0, 6, 13, 19, 
+			      25, 31, 38, 44, 
+			      50, 56, 63, 69,
+			      75, 81, 88, 94};
 
-
+// 定时器0，产生10ms中断，100次即为1s
 #define FOSC  11059200
 #define HZ    100
 #define T0MS  (65536 - FOSC/12/HZ)
 
-//__sfr AUXR = 0x8e; // STC 
-
 void init_timer()
 {
 	AUXR &= 0x7f;  // timer0 in 12T mode
-
-
 	TMOD = 0;      // all timer in mode 0 
-
 	TL0   = T0MS; 
 	TH0   = T0MS >> 8; 
-
 	TR0   = 1;
 	ET0   = 1;
+//	TF0   = 0;
 	EA    = 1;
 }
 
-// for SDCC, printf
-void putchar(char c) 
-{
-	TxSend(c);
-}
-
 char  times = 0;
-
-void timer0_handler() __interrupt __using 1
+void timer0_handler() __interrupt 1  __using 2
 {
-	times++;
-
-	if (HZ == times) {
+	if (HZ == times++) {
 		flag = 0x55;
 		times = 0;
 	}
-
-	P3_6 = !P3_6; // for test
 }
 
+// 打印十进制数字
+void print_num(unsigned char dat)
+{
+	char i;
+
+	for (i=0;i<3;i++) {
+		if (dat < 100) 
+			break;
+		dat -= 100;
+	}
+
+	if (i) 
+		putchar('0'+i);
+
+	for (i=0;i<10;i++) {
+		if (dat < 10) 
+			break;
+		dat -= 10;
+	}
+
+	putchar('0'+i);
+	putchar('0'+dat);
+}
 
 /*
- *
+ * 主程序
  */
 
 int main()
 {
-	unsigned int read_temp;
 	char h, l;
-	unsigned int count=0;
+	unsigned char count=0;
 
 	init_timer();
 
 	while(1) {
 		if (flag) {
+			P3_2 = !P3_2; // for test
 			flag = 0;
-#if 1
-			read_temp = ReadTemp();			
+			ReadTemp();
+
 			h = (TPH<<4) + ((TPL>>4) & 0x0f);
 			
 			if (h<0) 
-				l = 16 - (TPL&0xf);
+				l = digis[16 - (TPL&0xf)];
 			else
-				l = TPL&0xf;
-#else 
-			h = 1; l=1;
-#endif
+				l = digis[TPL&0xf];
 			
-			printf("%u\t%d.%s\n\r", count++,  h, digis[l]);
+			print_num(count++);
+			putchar('\t');
+			
+			if (h<0) {
+				putchar('-');
+				h = -h;
+			}
+			print_num(h);
+			
+			putchar('.');
+			print_num(l);
+			putchar('\n');
+			putchar('\r');
 		}
 	}
-}		
+}
+
