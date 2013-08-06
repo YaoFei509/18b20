909824b265b84213d7ff18b1f0dd3f014409dde5
Yao Fei <feiyao@me.com>
2013-08-07 00:10:35 +0800

    ReadTemp 不再返回整数

--- ds18b20_1t.h
+++ ds18b20_1t.h
@@ -10,25 +10,24 @@
 #ifndef __DS18B20_1T_H
 #define __DS18B20_1T_H
 
-#include <8052.h>
-//#include "STC15fxxxx.h"
+#include <stc12.h>
 
 typedef unsigned char BYTE;
 
-#define DQ P3_3                     /*DS18B20的数据口位P3.3*/
-extern BYTE TPH;                           //存放温度值的高字节
-extern BYTE TPL;                           //存放温度值的低字节
+#define DQ P3_3                    /*DS18B20的数据口位P3.3*/
+extern BYTE TPH;                   //存放温度值的高字节
+extern BYTE TPL;                   //存放温度值的低字节
 
 // for Keil C compatible
 #define __nop__    __asm  nop __endasm
 
-
+//------------------------------------------------------
 void DelayXus(BYTE n);
 void DS18B20_Reset();
 void DS18B20_WriteByte(BYTE dat);
 BYTE DS18B20_ReadByte();
 
-int  ReadTemp();
-
-
+// return value in TPH TPL
+// Yao Fei
+void ReadTemp();
 #endif
