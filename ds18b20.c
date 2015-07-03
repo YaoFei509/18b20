1c063a138eb4d02674d573812d4ff6cbb9d15021
姚飞 <feiyao@me.com>
2015-07-04 05:15:01 +0800

    跟随ds18b20_1t.c补充完善函数

--- ds18b20.c
+++ ds18b20.c
@@ -7,7 +7,7 @@
 // 检测并启动DS18B20转换
 // 返回0: 存在并已经启动
 //     1: 不存在DS18B20
-BYTE StartDS18B20()
+__bit StartDS18B20()
 {
 	if (DS18B20_Reset() == 0) {
 		DS18B20_WriteByte(0xCC);        //跳过ROM命令
@@ -56,28 +56,20 @@ void DelayX0us(BYTE n)
 /**************************************
 复位DS18B20,并检测设备是否存在
 **************************************/
-BYTE DS18B20_Reset()
+__bit DS18B20_Reset()
 {
-	BYTE i=0;
-	BYTE CY = 1;
+	__bit CY;
 	
-	while (CY && (i<100)) {
-		DQ = 1;
-		DelayX0us(2);
-		DQ = 0;                     //送出低电平复位信号
-		DelayX0us(48);              //延时至少480us
-		DQ = 1;                     //释放数据线
-		DelayX0us(6);               //等待60us
-		CY = DQ;                    //检测存在脉冲
-		DelayX0us(42);              //等待设备释放数据线
-		i++;
-	}
+	DQ = 1;
+	DelayX0us(2);
+	DQ = 0;                     //送出低电平复位信号
+	DelayX0us(48);              //延时至少480us
+	DQ = 1;                     //释放数据线
+	DelayX0us(6);               //等待60us
+	CY = DQ;                    //检测存在脉冲
+	DelayX0us(42);              //等待设备释放数据线
 
-	if (100 == i )
-		return 1;
-	else
-		return 0;  //found
-	
+	return CY; 	
 }
 
 /**************************************
@@ -134,4 +126,41 @@ BYTE DS18B20_ReadRom(BYTE *rom)
 		rom[i] = DS18B20_ReadByte();
 	}
 	return 0;
+}
+
+// 读一位
+__bit DS18B20_ReadBit()
+{
+	__bit result;
+
+	DQ = 0;            //开始时间片
+
+	__nop__;           //延时等待
+	__nop__;
+	__nop__;
+	__nop__;
+	
+        DQ = 1;                     //准备接收
+
+	__nop__;           //延时等待
+	__nop__;
+	__nop__;
+	__nop__;
+
+	result = DQ;
+
+	DelayX0us(6);               //等待时间片结束
+
+        return result;
+}
+
+void DS18B20_WriteBit(__bit dat)
+{
+	DQ = 0;                     //开始时间片
+	__nop__;                    //延时等待
+	__nop__;
+	dat >>= 1;                  //送出数据
+	DQ = dat;
+	DelayX0us(6);               //等待时间片结束
+	DQ = 1;                     //恢复数据线
 }
