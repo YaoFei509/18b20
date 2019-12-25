498dda054f7591a5c3e83bbedd98d4a95f3226cd
Yao Fei <feiyao@me.com>
2019-12-25 22:13:08 +0800

    send a 't' before read data

--- linux/read_temp.pl
+++ linux/read_temp.pl
@@ -1,6 +1,9 @@
 #!/usr/bin/perl
 use Time::Local;
 use DBI;
+use Device::SerialPort;
+
+my $port = Device::SerialPort->new("$ARGV[0]") || die ("Can't open serial port\n");
 
 my $database='yfhome';
 my $server  ='yfhomeserver.local'; 
@@ -10,16 +13,25 @@ my $password='www';
 my $dsn = "DBI:mysql:database=$database:host=$server";
 my $dbh = DBI->connect($dsn, $username, $password) || die "Can't connect.\n";
 
-while(<>) {
-	chop;
-	@line = split('\t');
-	next if $line[2] == "";
+$t = time;
+$port->baudrate(9600);
+$port->write("T");
 
-	$t = time;
-	if ( $t % 60 == 0 )  {  
-	$sql = qq[ INSERT home_temp VALUES  (0,$t,"$line[0]",$line[1], "$line[2]" ) ];
-	$sth    = $dbh->prepare($sql) || die "DBI error with connect to database.\n";
-	$result = $sth->execute       || die "DBI error with execute.\n";
-	}
-    }
+sleep 1; #wait for serialport!
+
+($cnt, $_) = $port->read(255);
+chop;
+chop;
+
+@line = split(/\t/);
+
+
+$sql = qq[ INSERT home_temp VALUES  (0,$t,0, $line[1], "$line[0]") ];
+
+print "$sql\n";
+
+$sth    = $dbh->prepare($sql) || die "DBI error with connect to database.\n";
+$result = $sth->execute       || die "DBI error with execute.\n";
+
 $dbh->disconnect;
+undef $port;
